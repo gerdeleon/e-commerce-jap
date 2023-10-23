3837db1a5ef8f86a3a6352c679cdd47533bce000
Ana Pérez <anaperezdecuadro@gmail.com>
2023-10-23 12:04:32 -0300

    Avance Lunes

--- js/cart.js
+++ js/cart.js
@@ -44,13 +44,13 @@ function crearProductoHTML(item) {
             <input type="number" id="cantidad-${id}" value="${soldCount}" min='0' max='100'">
             <p>Subtotal: <span id="subtotal-${id}">${cost * soldCount} </span></p>
             <img src="${image}" alt="Imagen del producto" width="300">
-            <button id="btnEliminar" class="btn btn-eliminar" data-product-id="${id}" style="background-color: rgb(255, 165, 0);">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash text-white" viewBox="0 0 16 16">
-    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-  </svg>
-</button>
- </div>
+            <button id="reducir-cantidad-${id}" class="btn btn-eliminar" data-product-id="${id}" style="background-color: rgb(255, 165, 0);">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash text-white" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                </svg>
+            </button>
+        </div>
     `;
 }
 
@@ -58,65 +58,97 @@ function actualizarUI(cartProducts) {
   const infoCarrito = document.getElementById("infoCarrito");
   const tipoEnvioRadios = document.getElementsByName("opcion");
 
+
   infoCarrito.innerHTML = cartProducts.map(crearProductoHTML).join('');
 
-  let subtotalGeneral = 0; // Inicializa el subtotal general en 0
+
+  let subtotalGeneral = 0;
   let total_del_precio = 0;
 
+
   for (let item of cartProducts) {
-      const { cost, id, soldCount } = item;
-      const cantidadInput = document.getElementById(`cantidad-${id}`);
+    const { cost, id, soldCount } = item;
+    const cantidadInput = document.getElementById(`cantidad-${id}`);
+    const reducirCantidadButton = document.getElementById(`reducir-cantidad-${id}`);
 
-      cantidadInput.addEventListener("input", function (event) {
-          const nuevaCantidad = parseInt(event.target.value);
-          const nuevoSubtotal = cost * nuevaCantidad;
-          const subtotalElement = document.getElementById(`subtotal-${id}`);
-          subtotalElement.textContent = nuevoSubtotal.toFixed(2);
+    reducirCantidadButton.addEventListener("click", function (event) {
+      reducirCantidad(id);
+  });
 
-          // Actualiza el subtotal general
-          subtotalGeneral = cartProducts.reduce((total, product) => {
-              return total + product.cost * (parseInt(document.getElementById(`cantidad-${product.id}`).value));
-          }, 0);
-          subtotalGeneralElement.textContent = subtotalGeneral.toFixed(2);
-          actualizarCostoEnvio(subtotalGeneral);
-      });
+  function reducirCantidad(productID) {
+    const cantidadInput = document.getElementById(`cantidad-${productID}`);
+    let cantidadActual = parseInt(cantidadInput.value);
 
-      total_del_precio += cost * soldCount;
+    // Verifica que la cantidad actual sea mayor que 0 antes de reducirla
+    if (cantidadActual > 0) {
+        cantidadActual--;
+        cantidadInput.value = cantidadActual;
+
+        // Calcula el nuevo subtotal y actualiza la vista
+        const nuevoSubtotal = calcularSubtotal(productID);
+        const subtotalElement = document.getElementById(`subtotal-${productID}`);
+        subtotalElement.textContent = nuevoSubtotal.toFixed(2);
+
+        // Después de reducir la cantidad, actualiza los totales
+        actualizarCostoEnvio(subtotalGeneral);
+    }
+}
+
+  
+
+    cantidadInput.addEventListener("input", function (event) {
+      const nuevaCantidad = parseInt(event.target.value);
+      const nuevoSubtotal = cost * nuevaCantidad;
+      const subtotalElement = document.getElementById(`subtotal-${id}`);
+      subtotalElement.textContent = nuevoSubtotal.toFixed(2);
+
+
+      // Actualiza el subtotal general
+      subtotalGeneral = cartProducts.reduce((total, product) => {
+        return total + product.cost * (parseInt(document.getElementById(`cantidad-${product.id}`).value));
+      }, 0);
+      subtotalGeneralElement.textContent = subtotalGeneral.toFixed(2);
+      actualizarCostoEnvio(subtotalGeneral);
+    });
+
+
+    total_del_precio += cost * soldCount;
   }
+
 
   const subtotalGeneralElement = document.getElementById("subtotal-general").querySelector("span");
   subtotalGeneralElement.textContent = total_del_precio.toFixed(2);
 
+
   const costoEnvioElement = document.getElementById("costo-envio").querySelector("span");
   // Agregar evento de escucha a los radios de tipo de envío
   for (let radio of tipoEnvioRadios) {
-      radio.addEventListener("change", function () {
-          actualizarCostoEnvio(subtotalGeneral);
-      });
+    radio.addEventListener("change", function () {
+      actualizarCostoEnvio(subtotalGeneral);
+    });
   }
-
+ 
   function actualizarCostoEnvio(subtotalGeneral) {
-      let costoEnvio = 0;
-      for (let radio of tipoEnvioRadios) {
-          if (radio.checked) {
-              const porcentajeEnvio = parseFloat(radio.value) / 100;
-              costoEnvio = subtotalGeneral * porcentajeEnvio;
-          }
+    let costoEnvio = 0;
+    for (let radio of tipoEnvioRadios) {
+      if (radio.checked) {
+        const porcentajeEnvio = parseFloat(radio.value) / 100;
+        costoEnvio = subtotalGeneral * porcentajeEnvio;
       }
-  costoEnvioElement.textContent = costoEnvio.toFixed(2);
+    }
+    costoEnvioElement.textContent = costoEnvio.toFixed(2);
 
-  const totalPagar = subtotalGeneral + costoEnvio;
-  const totalPagarElement = document.getElementById("total-pagar").querySelector("span");
-  totalPagarElement.textContent = totalPagar.toFixed(2);
+
+    const totalPagar = subtotalGeneral + costoEnvio;
+    const totalPagarElement = document.getElementById("total-pagar").querySelector("span");
+    totalPagarElement.textContent = totalPagar.toFixed(2);
+  }
 }
-};
-
-
 
 
 /// DESAFIATEE Agregar evento de click al botón "Eliminar" ///
 
-function eliminarProducto(productoId) {
+/* function eliminarProducto(productoId) {
     const cartProducts = JSON.parse(localStorage.getItem('carrito')) || [];
 
     // Encontrar el índice del producto a eliminar por su ID
@@ -131,48 +163,10 @@ function eliminarProducto(productoId) {
 
         // Recalcular los totales y actualizar la interfaz de usuario
         actualizarUI(cartProducts);
-    }
-}
+    } */
+
+
 /// FIN DESAFIATEE ///
     
-    ////////////////////////////////////////////////
-// Agrega el evento "click" al botón con ID "btnEliminar"
-document.getElementById('btnEliminar').addEventListener('click', function () {
-    // Obtén el ID del producto a eliminar del atributo "data-product-id"
-    const productIdToDelete = this.getAttribute('data-product-id');
-  
-    // Llama a la función para eliminar el producto del carrito
-    eliminarProducto(productIdToDelete);
-  });
-  
-  function eliminarProducto(productId) {
-    let lsCarritoData = JSON.parse(localStorage.getItem('carrito'));
-  
-    if (!lsCarritoData) {
-      // El carrito está vacío, no hay nada que eliminar.
-      return;
-    }
-  
-    let itemDelLocal = lsCarritoData.find((item) => item.id === productId);
-  
-    if (itemDelLocal) {
-      if (itemDelLocal.soldCount > 1) {
-        // Si hay más de un artículo del mismo tipo en el carrito, decrementa la cantidad.
-        const prod = {
-          ...itemDelLocal,
-          soldCount: itemDelLocal.soldCount - 1
-        };
-        let filteredCart = lsCarritoData.filter((item) => item.id !== productId);
-        localStorage.removeItem('carrito');
-        localStorage.setItem('carrito', JSON.stringify([...filteredCart, prod]));
-      } else {
-        // Si solo hay un artículo del tipo en el carrito, elimínalo completamente.
-        let filteredCart = lsCarritoData.filter((item) => item.id !== productId);
-        localStorage.removeItem('carrito');
-        localStorage.setItem('carrito', JSON.stringify(filteredCart));
-      }
-  
-      // Llama a la función para actualizar la interfaz después de eliminar el producto.
-      actualizarUI(filteredCart);
-    }
-  }
+
+
